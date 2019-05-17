2b92b167c23d98197eeb2f81df77b9f1a2a47cb6
Maryna_Khvashcheuska <Maryna_Khvashcheuska@EPBYMINW8690>
2019-05-17 18:33:02 +0300

    fix issues

--- src/components/CustomComponent.jsx
+++ src/components/CustomComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class CustomComponent extends React.Component {
+export class CustomComponent extends React.Component {
     render() {
         return (
             <div style={this.props.style}>{this.props.text}</div>
--- src/components/CustomPureComponent.jsx
+++ src/components/CustomPureComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class CustomPureComponent extends React.PureComponent {
+export class CustomPureComponent extends React.PureComponent {
     render() {
         return (
             <div style={this.props.style}>{this.props.text}</div>
